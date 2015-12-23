14549c224e6ffa965dda6544a0099f997418593a
David Brenner <jetsfandb@gmail.com>
2015-12-23 08:48:47 -0500

    Battery efficiency changes.
    Changed TickTime service back to minute units. (Discovered that it fires when the minute value of the clock changes). 
    Added static variables for battery and bluetooth states.
    Added subscription to battery and bluetooth services so that the static state variables are only updated on a change.
    Commented out call to destroy_window during deinit(). Forum comments seem to suggest it is not needed since the OS does this and in fact creates a double free error condition.

--- src/main.c
+++ src/main.c
@@ -2,17 +2,27 @@
 #include "main.h"
 
 
-static Window    *s_main_window;
-static TextLayer *s_dow_text_layer;
-static TextLayer *s_time_text_layer;
-static TextLayer *s_date_text_layer;
-static TextLayer *s_battery_text_layer;
-static TextLayer *s_bluetooth_text_layer;
+static Window      *s_main_window;
+static TextLayer   *s_dow_text_layer;
+static TextLayer   *s_time_text_layer;
+static TextLayer   *s_date_text_layer;
+static BitmapLayer *s_battery_image_layer;
+static TextLayer   *s_battery_pct_layer[10];
+static TextLayer   *s_bluetooth_text_layer;
+static BitmapLayer *s_bluetooth_conn_layer;
 
+static GBitmap   *s_battery_base;
+static GBitmap   *s_icon_bluetooth_connected;
+static GBitmap   *s_icon_bluetooth_disconnected;
 static GFont      s_normal_font;
 static GFont      s_big_font;
 
+static BatteryChargeState s_battery_charge_state;
+static bool s_bluetooth_connected;
+
 static void main_window_load(Window *window) {
+  s_battery_charge_state = battery_state_service_peek();
+  s_bluetooth_connected  = bluetooth_connection_service_peek();
   Layer *window_layer = window_get_root_layer(s_main_window);
   s_normal_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
   s_big_font    = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK);
@@ -20,108 +30,148 @@ static void main_window_load(Window *window) {
   // Setup each text layer
     
   GRect window_bounds = layer_get_bounds(window_layer);
+  GRect layer_bounds;
+  layer_bounds.origin.x = 0;
+  layer_bounds.size.w = window_bounds.size.w;
+  s_battery_base                = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY_BASE);  
+  s_icon_bluetooth_connected    = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLUETOOTH_CONNECTED);  
+  s_icon_bluetooth_disconnected = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLUETOOTH_DISCONNECTED);  
   
   //Dow
-  s_dow_text_layer = text_layer_create(window_bounds);
+  layer_bounds.origin.y = 0;
+  layer_bounds.size.h = 26;
+  s_dow_text_layer = text_layer_create(layer_bounds);
   text_layer_set_font(s_dow_text_layer, s_normal_font);
   text_layer_set_text_alignment(s_dow_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_dow_text_layer));  
   
+  //Date
+  layer_bounds.origin.y = (layer_bounds.origin.y + layer_bounds.size.h);
+  layer_bounds.size.h = 26;
+  s_date_text_layer = text_layer_create(layer_bounds);
+  text_layer_set_font(s_date_text_layer, s_normal_font);
+  text_layer_set_text_alignment(s_date_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_date_text_layer));  
+  
   //Time
-  s_time_text_layer = text_layer_create(window_bounds);
+  layer_bounds.origin.y = (layer_bounds.origin.y + layer_bounds.size.h);
+  layer_bounds.size.h = 40;
+  s_time_text_layer = text_layer_create(layer_bounds);
   text_layer_set_font(s_time_text_layer, s_big_font);
   text_layer_set_text_alignment(s_time_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_time_text_layer));  
   
-  //Date
-  s_date_text_layer = text_layer_create(window_bounds);
-  text_layer_set_font(s_date_text_layer, s_normal_font);
-  text_layer_set_text_alignment(s_date_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_date_text_layer));  
+  //Battery - Image
+  layer_bounds.origin.y = (layer_bounds.origin.y + layer_bounds.size.h);
+  layer_bounds.size.h = 10;
+  s_battery_image_layer = bitmap_layer_create(layer_bounds);
+  bitmap_layer_set_bitmap(s_battery_image_layer, s_battery_base);
+  bitmap_layer_set_alignment(s_battery_image_layer, GAlignCenter);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_battery_image_layer));  
 
-  //Battery
-  s_battery_text_layer = text_layer_create(window_bounds);
-  text_layer_set_font(s_battery_text_layer, s_normal_font);
-  text_layer_set_text_alignment(s_battery_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_battery_text_layer));  
-
+  //Battery - Percentages
+  GRect pct_bound;  
+  int base_x_offset  = 6;
+  pct_bound.origin.y = 2;
+  pct_bound.size.w   = 12;
+  pct_bound.size.h   = 6;
+  
+  for(int x = 0; x < 10; x++) {
+    pct_bound.origin.x = (base_x_offset + (pct_bound.size.w * x) + (1 + x));
+    s_battery_pct_layer[x] = text_layer_create(pct_bound);
+    layer_add_child(bitmap_layer_get_layer(s_battery_image_layer), text_layer_get_layer(s_battery_pct_layer[x]));      
+  }
+  
   //Bluetooth
-  s_bluetooth_text_layer = text_layer_create(window_bounds);
+  layer_bounds.origin.y = (layer_bounds.origin.y + layer_bounds.size.h);
+  GRect bluetooth_label_rect = layer_bounds;  
+  bluetooth_label_rect.origin.x = 2;
+  bluetooth_label_rect.size.h = 26;
+  bluetooth_label_rect.size.w = 120;
+  s_bluetooth_text_layer = text_layer_create(bluetooth_label_rect);
   text_layer_set_font(s_bluetooth_text_layer, s_normal_font);
   text_layer_set_text_alignment(s_bluetooth_text_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_bluetooth_text_layer, "Bluetooth:");
   layer_add_child(window_layer, text_layer_get_layer(s_bluetooth_text_layer));  
 
+  GRect bluetooth_state_rect = layer_bounds;  
+  bluetooth_state_rect.origin.x = (bluetooth_label_rect.origin.x + bluetooth_label_rect.size.w);
+  bluetooth_state_rect.size.h = 26;
+  bluetooth_state_rect.size.w = 20;
+  s_bluetooth_conn_layer = bitmap_layer_create(bluetooth_state_rect);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bluetooth_conn_layer));
 }
 
 static void main_window_unload(Window *window) {
-    text_layer_destroy(s_dow_text_layer);
-    text_layer_destroy(s_time_text_layer);
-    text_layer_destroy(s_date_text_layer);
-    text_layer_destroy(s_battery_text_layer);
-    text_layer_destroy(s_bluetooth_text_layer);
+  for(int x = 0; x < 10; x++) {
+    text_layer_destroy(s_battery_pct_layer[x]);
+  }
+  text_layer_destroy(s_dow_text_layer);
+  text_layer_destroy(s_time_text_layer);
+  text_layer_destroy(s_date_text_layer);
+  gbitmap_destroy(s_battery_base);
+  gbitmap_destroy(s_icon_bluetooth_connected);
+  gbitmap_destroy(s_icon_bluetooth_disconnected);
+  bitmap_layer_destroy(s_battery_image_layer);
+  text_layer_destroy(s_bluetooth_text_layer);
+  bitmap_layer_destroy(s_bluetooth_conn_layer);
 }
 
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
-  GRect prev_bound;
 
   // Day of Week
   static char s_dow[11];
   strftime(s_dow, sizeof(s_dow), "%A", tick_time);
   text_layer_set_text(s_dow_text_layer, s_dow);
-  GRect new_bound = layer_get_bounds(text_layer_get_layer(s_dow_text_layer));
-  new_bound.size.h = 14;
-  layer_set_bounds(text_layer_get_layer(s_dow_text_layer), new_bound);
   
   // Date
   static char s_date[12];
   strftime(s_date, sizeof(s_date), "%B %e", tick_time);
   text_layer_set_text(s_date_text_layer, s_date);
-  prev_bound = new_bound;
-  new_bound = layer_get_bounds(text_layer_get_layer(s_date_text_layer));
-  new_bound.origin.y = (prev_bound.origin.y + prev_bound.size.h);
-  new_bound.size.h = 14;
-  layer_set_bounds(text_layer_get_layer(s_date_text_layer), new_bound);
   
   // Time
   static char s_time[9];
   strftime(s_time, sizeof(s_time), "%l:%M %p", tick_time);
   text_layer_set_text(s_time_text_layer, s_time);
-  prev_bound = new_bound;
-  new_bound = layer_get_bounds(text_layer_get_layer(s_time_text_layer));
-  new_bound.origin.y = (prev_bound.origin.y + prev_bound.size.h);
-  new_bound.size.h = 20;
-  layer_set_bounds(text_layer_get_layer(s_time_text_layer), new_bound);
   
-  // Battery
-  static char s_battery[30];
-  BatteryChargeState bcs = battery_state_service_peek();
-  snprintf(s_battery, sizeof(s_battery), "Battery: %d%%", bcs.charge_percent);
-  text_layer_set_text(s_battery_text_layer, s_battery);
-  prev_bound = new_bound;
-  new_bound = layer_get_bounds(text_layer_get_layer(s_battery_text_layer));
-  new_bound.origin.y = (prev_bound.origin.y + prev_bound.size.h);
-  new_bound.size.h = 15;
-  layer_set_bounds(text_layer_get_layer(s_battery_text_layer), new_bound);
+  
+  // Battery - Image
+  int battery_pct_tenths = (s_battery_charge_state.charge_percent / 10);
+  GColor battery_pct_color;
+  if(battery_pct_tenths > 6) {
+    battery_pct_color = GColorGreen;
+  }
+  else if (battery_pct_tenths > 3) {
+    battery_pct_color = GColorChromeYellow;    
+  }
+  else {
+    battery_pct_color = GColorRed;
+  }
+  for(int x = 0; x < 10; x++) {
+    if(x < battery_pct_tenths) {
+      text_layer_set_background_color(s_battery_pct_layer[x], battery_pct_color);
+    }
+    else {
+      text_layer_set_background_color(s_battery_pct_layer[x], GColorWhite);      
+    }
+  }
   
   // Bluetooth
-  static char s_bluetooth[30];
-  bool bt_connected = bluetooth_connection_service_peek();
-  snprintf(s_bluetooth, sizeof(s_bluetooth), "Bluetooth: %s", bt_connected ? "Yes" : "No");
-  text_layer_set_text(s_bluetooth_text_layer, s_bluetooth);
-  prev_bound = new_bound;
-  new_bound = layer_get_bounds(text_layer_get_layer(s_bluetooth_text_layer));
-  new_bound.origin.y = (prev_bound.origin.y + prev_bound.size.h);
-  new_bound.size.h = 15;
-  layer_set_bounds(text_layer_get_layer(s_bluetooth_text_layer), new_bound);
+  bitmap_layer_set_bitmap(s_bluetooth_conn_layer, s_bluetooth_connected? s_icon_bluetooth_connected : s_icon_bluetooth_disconnected);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
-
+static void battery_state_handler(BatteryChargeState bcs) {
+  s_battery_charge_state = bcs;
+}
+static void bluetooth_state_handler(bool connected) {
+  s_bluetooth_connected = connected;
+}
 static void init() {
   s_main_window = window_create();  
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -130,20 +180,21 @@ static void init() {
   });
   window_stack_push(s_main_window, true);
   
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  battery_state_service_subscribe(battery_state_handler);
+  bluetooth_connection_service_subscribe(bluetooth_state_handler);
   update_time();
 }
 
 
 static void deinit() {
     tick_timer_service_unsubscribe();
-    window_destroy(s_main_window);
+    battery_state_service_unsubscribe();
+    bluetooth_connection_service_unsubscribe();
+  //  window_destroy(s_main_window);
 }
 int main(void) {
   init();
   app_event_loop();
   deinit();
 }
-
-
-
